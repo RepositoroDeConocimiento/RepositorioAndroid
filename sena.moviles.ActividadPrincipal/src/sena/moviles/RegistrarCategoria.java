00ce5040bb07659198819e5ac24dec7c5b7e370a
Diego <Diego@10.71.135.15>
2012-11-06 20:40:52 -0500

    Commit proyecto Android

--- sena.moviles.ActividadPrincipal/src/sena/moviles/RegistrarCategoria.java
+++ sena.moviles.ActividadPrincipal/src/sena/moviles/RegistrarCategoria.java
@@ -1,10 +1,18 @@
 package sena.moviles;
 
+import sena.moviles.connection.RepositorioSQLiteHelper;
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistrarCategoria extends Activity {
+	
+	EditText cajaNom;
+	EditText cajaDescripcion;
+	
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -12,6 +20,36 @@ public class RegistrarCategoria extends Activity {
         setContentView(R.layout.crear_categoria);
        
     }
+	
+	public void registrar(View view)
+	{
+		 //Abrimos la base de datos 'DBUsuarios' en modo escritura
+        RepositorioSQLiteHelper usdbh =
+	            new RepositorioSQLiteHelper(this, "DBRepositorioAprendizaje", null, 1);
+ 
+        SQLiteDatabase db = usdbh.getWritableDatabase();
+        //almacenarRegistros(db,);
+ 
+        //Si hemos abierto correctamente la base de datos
+        if(db != null)
+        {
+        	//usdbh.onUpgrade(db, 1, 2);
+        	cajaNom= (EditText) findViewById(R.id.campoNombre);
+        	cajaDescripcion= (EditText) findViewById(R.id.areaDescripcion);
+        	int documento=0;
+        	        	
+        	documento=Integer.parseInt("23");
+        	db.execSQL("INSERT INTO categoria (nombre, descripcion) " +
+               "VALUES ('" + cajaNom.getText().toString() + "', '" + 
+        			cajaDescripcion.getText().toString() + "')");
+        	
+         		    		 
+    		 Toast.makeText(RegistrarCategoria.this, "despues del registro", Toast.LENGTH_SHORT).show();
+    		 //se cierra la BD
+            db.close();
+        }
+        
+	}
 	
 	public void cerrar(View view) {
     	finish();
--- sena.moviles.ActividadPrincipal/src/sena/moviles/VentanaPrincipal.java
+++ sena.moviles.ActividadPrincipal/src/sena/moviles/VentanaPrincipal.java
@@ -3,16 +3,18 @@ package sena.moviles;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class VentanaPrincipal extends Activity {
 	
 	TextView rol;
-	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ventana_principal);
@@ -21,14 +23,16 @@ public class VentanaPrincipal extends Activity {
         rol.setText(bundle.getString("VentanaPrincipal").toString());
     }
 
-
-    public boolean onCreateOptionsMenu(Menu menu){
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
 		MenuInflater inflater=getMenuInflater();
 		inflater.inflate(R.menu.menu_principal, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
 	
+
 	
+	@Override
 	public boolean onOptionsItemSelected(MenuItem item){
 		
 		switch (item.getItemId()) {
@@ -48,6 +52,8 @@ public class VentanaPrincipal extends Activity {
 			break;
 		case R.id.subItemGestionarCategoria:
 			Toast.makeText(VentanaPrincipal.this, "Selecciono Gestionar Categoria", Toast.LENGTH_SHORT).show();
+			Intent j = new Intent(this, ConsultarCategoria.class );
+		    startActivity(j);
 			break;
 		default:
 			break;
